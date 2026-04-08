e838c58ecdec4509601be12777c1aa3722acc416
abhirup <abhirupraj777@gmail.com>
2026-04-08 20:02:32 +0530

    commit sum  changes

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class App {
+	public String sayHello() { return "Hello World"; }
+	// Add this inside the public class App { ... }
+	public int sum(int a, int b) {
+	    return a + b;
+	}
+
+    public static void main(String[] args) {
+        System.out.println(new App().sayHello());
+    }
+
+}
