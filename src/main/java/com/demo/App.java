a186476b34c8cd260381080ae1c2d37ea4424ea0
abhirup <abhirupraj777@gmail.com>
2026-04-11 19:53:11 +0530

    Division Feature

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -18,5 +18,13 @@ public class App {
     public int subtract(int a, int b) {
         return a - b;
     }
+    
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
 
 }
